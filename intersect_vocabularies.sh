40cc59fb39d9955c1eccc1ea6cc6ef80fbca1c52
Alvaro <lvapeab@gmail.com>
2016-11-11 15:16:10 +0100

    Fixbug

--- intersect_vocabularies.sh
+++ intersect_vocabularies.sh
@@ -19,17 +19,25 @@ cat $file2 |  tr " " '\n' | sort -u > /tmp/voc2
 
 
 
-union_size=`cat /tmp/voc1 /tmp/voc2| tr " " '\n' | sort -u |wc -l`
+union_size=`cat /tmp/voc1 /tmp/voc2| tr " " '\n' | sort -u | wc -l`
 intersection_size=`grep -Fxf /tmp/voc1 /tmp/voc2 | sort -u | wc -l`
 
 
-vocab1_size=`wc  -l /tmp/voc1 `
-vocab2_size=`wc -l /tmp/voc2`
+vocab1_size=`wc -l /tmp/voc1 | awk '{print $1}'`
+vocab2_size=`wc -l /tmp/voc2 | awk '{print $1}'`
+
+intersection_ratio1=`echo "scale=4;  ${intersection_size}/${vocab1_size}" | bc -l`
+intersection_ratio2=`echo "scale=4;  ${intersection_size}/${vocab2_size}" | bc -l`
 
 
-echo "$file1 vocabulary size: $vocab1_size"
-echo "$file2 vocabulary size: $vocab2_size"
-echo "$file1 ∪ $file2 vocabulary size: $union_size"
-echo "$file1 ∩ $file2 vocabulary size: $intersection_size"
+echo "${file1} vocabulary size: ${vocab1_size}"
+echo "${file2} vocabulary size: ${vocab2_size}"
+echo "${file1} ∪ ${file2} vocabulary size: ${union_size}"
+echo "${file1} ∩ ${file2} vocabulary size: ${intersection_size}"
+
+echo "Which accounts for: "
+echo -e "\t 0${intersection_ratio1} % of the vocabulary from ${file1}"
+echo -e "\t 0${intersection_ratio2} % of the vocabulary from ${file2}"
+
 
 #rm /tmp/voc1 /tmp/voc2
