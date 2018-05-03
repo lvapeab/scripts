572a719e153474e0394b82cec1d4a9b1e5f8e52f
lvapeab <lvapeab@gmail.com>
2018-05-03 11:01:46 +0200

    Better tokenize

--- tokenize.sh
+++ tokenize.sh
@@ -4,7 +4,7 @@ tok_path=/home/lvapeab/smt/software/mosesdecoder/scripts/tokenizer
 if [ $# -lt 1 ] 
 then 
     echo "Usage: $0 language input_file output_file"
-    echo "Computes the vocabulary size of text_file"
+    echo "Computes tokenizes the input_file. Outputs the result in output_file"
     exit 1
 fi
 
@@ -19,8 +19,11 @@ echo "Removing non printing chars"
 ${tok_path}/remove-non-printing-char.perl <  /tmp/${tmp_name} > /tmp/${tmp_name}_0
 echo "Replacing unicode punctuation"
 ${tok_path}/replace-unicode-punctuation.perl < /tmp/${tmp_name}_0 > /tmp/${tmp_name}_2
+echo "Normalizing punctuation"
+${tok_path}/normalize-punctuation.perl ${language} <  /tmp/${tmp_name}_2 | > /tmp/${tmp_name}_3
 echo "Tokenizing"
-${tok_path}/tokenizer.perl -threads 8 -l $language < /tmp/${tmp_name}_2 > ${output}
+${tok_path}/tokenizer.perl -threads 8 -l $language -aq  < /tmp/${tmp_name}_3 > ${output}
+
 echo "Done"
 rm /tmp/${tmp_name}*
  
