3058d7bf66b5dbc7d6a110c4aecc6b0a612f67cb
lvapeab <lvapeab@gmail.com>
2018-05-03 11:23:56 +0200

    Detect original codification of input file

--- tokenize.sh
+++ tokenize.sh
@@ -13,16 +13,17 @@ input=$2
 output=$3
 
 tmp_name=`basename $output`
+original_charset=`file -bi $input | awk 'BEGIN{FS="="}{print $2}'`
 echo "Converting to UTF-8"
-iconv -t UTF-8 $input > /tmp/${tmp_name}
+iconv -f ${original_charset} -t UTF-8 $input > /tmp/${tmp_name}
 echo "Removing non printing chars"
 ${tok_path}/remove-non-printing-char.perl <  /tmp/${tmp_name} > /tmp/${tmp_name}_0
 echo "Replacing unicode punctuation"
 ${tok_path}/replace-unicode-punctuation.perl < /tmp/${tmp_name}_0 > /tmp/${tmp_name}_2
 echo "Normalizing punctuation"
-${tok_path}/normalize-punctuation.perl ${language} <  /tmp/${tmp_name}_2 | > /tmp/${tmp_name}_3
+${tok_path}/normalize-punctuation.perl ${language} <  /tmp/${tmp_name}_2 > /tmp/${tmp_name}_3
 echo "Tokenizing"
-${tok_path}/tokenizer.perl -threads 8 -l $language -aq  < /tmp/${tmp_name}_3 > ${output}
+${tok_path}/tokenizer.perl -threads 8 -l $language -aq -no-escape  < /tmp/${tmp_name}_3 > ${output}
 
 echo "Done"
 rm /tmp/${tmp_name}*
